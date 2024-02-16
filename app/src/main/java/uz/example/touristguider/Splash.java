bf07aad5351ccabd3384dfa3691315d97346092c
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-02-17 00:16:10 +0500

    update

---
+++ app/src/main/java/uz/example/touristguider/Splash.java
@@ -0,0 +1,25 @@
+package uz.example.touristguider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(Splash.this, MainActivity.class));
+                    finish();
+                }
+            }, 1500);
+    }
+}
