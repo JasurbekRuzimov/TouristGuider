58d94def423cc23510cd0653dfc2ecfd9f0fbecc
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-02-18 23:12:34 +0500

    Main activity update

--- app/src/main/java/uz/example/touristguider/MainActivity.java
+++ app/src/main/java/uz/example/touristguider/MainActivity.java
@@ -1,14 +1,57 @@
 package uz.example.touristguider;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import uz.example.touristguider.adapters.CityAdapter;
+import uz.example.touristguider.bottomnavfragments.HomeFragment;
+import uz.example.touristguider.bottomnavfragments.LocationFragment;
+import uz.example.touristguider.bottomnavfragments.SaveFragment;
+import uz.example.touristguider.bottomnavfragments.SettingsFragment;
+import uz.example.touristguider.databinding.ActivityMainBinding;
+import uz.example.touristguider.models.City;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        uz.example.touristguider.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new HomeFragment());
+        fragmentTransaction.commit();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment;
+            if (item.getItemId() == R.id.menuHome) {
+                selectedFragment = new HomeFragment();
+            } else if (item.getItemId() == R.id.menuMap) {
+                selectedFragment = new LocationFragment();
+            } else if (item.getItemId() == R.id.menuSave) {
+                selectedFragment = new SaveFragment();
+            } else {
+                selectedFragment = new SettingsFragment();
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        });
+
+
     }
 }
