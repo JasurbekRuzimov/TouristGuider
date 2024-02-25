557879d54cadfc8a0922316da31bec62df2972e4
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-02-25 23:32:36 +0500

    Splash added

--- app/src/main/java/uz/example/touristguider/Splash/SplashMidd.java
+++ app/src/main/java/uz/example/touristguider/Splash/SplashMidd.java
@@ -2,9 +2,13 @@ package uz.example.touristguider.Splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import uz.example.touristguider.MainActivity;
 import uz.example.touristguider.R;
+import uz.example.touristguider.register.SignIn;
 
 public class SplashMidd extends AppCompatActivity {
 
@@ -12,5 +16,17 @@ public class SplashMidd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_midd);
+        if (isUserLoggedIn()) {
+            startActivity(new Intent(SplashMidd.this, MainActivity.class));
+        } else {
+            startActivity(new Intent(SplashMidd.this, SignIn.class));
+        }
+        finish();
     }
+
+    private boolean isUserLoggedIn() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        return sharedPreferences.getBoolean("isLoggedIn", false);
+    }
+
 }
