81c60f2e6a407a8c7645350468886566b019a9f7
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-02-25 23:32:06 +0500

    usermodel  updated

--- app/src/main/java/uz/example/touristguider/models/UserModel.java
+++ app/src/main/java/uz/example/touristguider/models/UserModel.java
@@ -1,2 +1,36 @@
-package uz.example.touristguider.models;public class UserModel {
+package uz.example.touristguider.models;
+
+public class UserModel {
+    String email, password, phoneNumber;
+
+
+    public UserModel(String email, String password, String phoneNumber) {
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
