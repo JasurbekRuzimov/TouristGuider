679de5356e17beadb28dfc0079638eb8374cfb63
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-02-25 23:39:40 +0500

    SignUp updated

--- app/src/main/java/uz/example/touristguider/register/SignUp.java
+++ app/src/main/java/uz/example/touristguider/register/SignUp.java
@@ -2,15 +2,86 @@ package uz.example.touristguider.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
-import uz.example.touristguider.R;
+import java.util.Objects;
+
+import uz.example.touristguider.databinding.ActivitySignUpBinding;
+import uz.example.touristguider.models.UserModel;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class SignUp extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        binding.signUpButtonFirebase.setOnClickListener(v -> {
+
+            String email = Objects.requireNonNull(binding.emailIdFairbase.getEditText()).getText().toString();
+            if (email.isEmpty()) {
+                binding.emailIdFairbase.setError("Emailni kiriting !");
+                return;
+            }
+            String phoneNumber = Objects.requireNonNull(binding.phoneNumberIdFirebase.getEditText()).getText().toString();
+            if (phoneNumber.isEmpty()) {
+                binding.phoneNumberIdFirebase.setError("Telefon raqam kiriting !");
+                return;
+            }
+            String password = Objects.requireNonNull(binding.passwordIdFairbase.getEditText()).getText().toString();
+            if (password.isEmpty()) {
+                binding.passwordIdFairbase.setError("Parolni kiriting !");
+                return;
+            }
+
+            progressDialog.show();
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnSuccessListener(authResult -> {
+                        startActivity(new Intent(SignUp.this, SignIn.class));
+                        progressDialog.cancel();
+
+                        firebaseFirestore.collection("User")
+                                .document(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())
+                                .set(new UserModel(email, password, phoneNumber));
+
+                    })
+                    .addOnFailureListener(e -> {
+                        Toast.makeText(SignUp.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        progressDialog.cancel();
+                    });
+        });
+
+        binding.alreadyHaveAnAccount.setOnClickListener(v -> {
+            startActivity(new Intent(SignUp.this, SignIn.class));
+            finish();
+        });
+
+        binding.alreadyHaveAnAccount1.setOnClickListener(v -> {
+            startActivity(new Intent(SignUp.this, SignIn.class));
+            finish();
+        });
+
+
     }
 }
+
+
