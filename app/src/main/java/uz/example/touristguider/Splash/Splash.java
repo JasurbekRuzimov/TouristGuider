d9ee5cb90f63f7165186f170f9bbf2cd615a0dd8
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-02-25 23:32:49 +0500

    Splash updated

--- app/src/main/java/uz/example/touristguider/Splash/Splash.java
+++ app/src/main/java/uz/example/touristguider/Splash/Splash.java
@@ -8,21 +8,21 @@ import android.os.Handler;
 
 import uz.example.touristguider.MainActivity;
 import uz.example.touristguider.R;
+import uz.example.touristguider.databinding.ActivitySplashBinding;
+import uz.example.touristguider.register.SignIn;
 
 public class Splash extends AppCompatActivity {
-
+ActivitySplashBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
             Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    startActivity(new Intent(Splash.this, MainActivity.class));
-                    finish();
-                }
+            handler.postDelayed(() -> {
+                startActivity(new Intent(Splash.this, SplashMidd.class));
+                finish();
             }, 1500);
     }
 }
