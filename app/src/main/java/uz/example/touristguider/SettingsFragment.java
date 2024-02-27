b3a76ab403ed467958f5ab6bf465e14733e59978
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-02-27 23:20:13 +0500

    settings fragment java added

---
+++ app/src/main/java/uz/example/touristguider/SettingsFragment.java
@@ -0,0 +1,91 @@
+package uz.example.touristguider;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import uz.example.touristguider.databinding.FragmentSettingsBinding;
+import uz.example.touristguider.register.SignIn;
+
+public class SettingsFragment extends Fragment {
+
+    FragmentSettingsBinding binding;
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        binding.backTv.setOnClickListener(v -> {
+            getActivity().onBackPressed();
+        });
+
+        binding.nightModeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+               setNightMode(true);
+            } else {
+               setNightMode(false);
+            }
+        });
+
+        binding.logout.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+            builder.setMessage(R.string.accountdan_chiqib_ketmoqchimisiz)
+                    .setPositiveButton("Yes", (dialog, id) -> {
+                        ProgressDialog progressDialog = new ProgressDialog(requireContext());
+                        progressDialog.setMessage(getString(R.string.chiqish_amalga_oshmoqda));
+                        progressDialog.show();
+                        FirebaseAuth.getInstance().signOut();
+                        progressDialog.dismiss();
+                        startActivity(new Intent(requireContext(), SignIn.class));
+                        requireActivity().finish();
+                    })
+                    .setNegativeButton("No", (dialog, id) -> dialog.dismiss());
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        });
+
+        binding.helpTv.setOnClickListener(v -> {
+            String tg_url = "https://t.me/Haytboyev_Sarvar";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(android.net.Uri.parse(tg_url));
+            startActivity(intent);
+        });
+
+        return view;
+    }
+
+    private void setNightMode(boolean b) {
+        if (b) {
+            requireActivity().setTheme(R.style.Theme_TouristGuiderDark);
+        } else {
+            requireActivity().setTheme(R.style.Theme_TouristGuider);
+        }
+    }
+}
