a0646619c9a45ae86e8559d7b836e7f2fb717ec5
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-02-28 23:42:35 +0500

    gradle updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
