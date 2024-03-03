93f634fe1f569ebd27a7b582d912889ab4778bbe
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-03-03 23:34:58 +0500

    Account fragment updated

--- app/src/main/java/uz/example/touristguider/bottomnavfragments/AccountFragment.java
+++ app/src/main/java/uz/example/touristguider/bottomnavfragments/AccountFragment.java
@@ -8,13 +8,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -22,7 +26,7 @@ import com.google.firebase.storage.StorageReference;
 import java.util.Objects;
 
 import uz.example.touristguider.R;
-import uz.example.touristguider.SettingsFragment;
+import uz.example.touristguider.settings.SettingsFragment;
 import uz.example.touristguider.databinding.FragmentAccountBinding;
 import uz.example.touristguider.settings.EditProfileFragment;
 
@@ -31,11 +35,11 @@ public class AccountFragment extends Fragment {
     private static final int PICK_IMAGE_REQUEST = 1;
     private FragmentAccountBinding binding;
     private FirebaseAuth auth;
-    private FirebaseFirestore firestore;
     private StorageReference storageReference;
+    String userId;
+    ProgressBar progressBar;
 
     public AccountFragment() {
-        // Required empty public constructor
     }
 
     public static AccountFragment newInstance() {
@@ -54,7 +58,6 @@ public class AccountFragment extends Fragment {
         binding = FragmentAccountBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // Initialize Firebase Auth and Storage
         auth = FirebaseAuth.getInstance();
         storageReference = FirebaseStorage.getInstance().getReference("profile_images");
 
@@ -76,12 +79,6 @@ public class AccountFragment extends Fragment {
             transaction.commit();
         });
 
-        binding.changeProfilePhoto.setOnClickListener(view -> {
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("image/*");
-            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-        });
-
         if (auth.getCurrentUser() != null) {
             binding.userEmail.setText(auth.getCurrentUser().getEmail());
             binding.userPassword.setText("**********");
@@ -89,7 +86,51 @@ public class AccountFragment extends Fragment {
             Toast.makeText(requireContext(), "User not found", Toast.LENGTH_SHORT).show();
         }
 
+        progressBar = binding.progressBarProfile;
+        userId = auth.getCurrentUser().getUid();
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.collection("users").document(userId).get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    progressBar.setVisibility(View.VISIBLE);
+                    if (documentSnapshot.exists()) {
+                        String name = documentSnapshot.getString("name");
+                        String surname = documentSnapshot.getString("surname");
+                        String country = documentSnapshot.getString("country");
+                        String number = documentSnapshot.getString("phoneNumber");
+                        binding.userName.setText(name);
+                        binding.userSurname.setText(surname);
+                        binding.userCountry.setText(country);
+                        binding.userPhone.setText(number);
+                        progressBar.setVisibility(View.GONE);
+                    } else {
+                        Toast.makeText(requireContext(), "User not found", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(e -> Toast.makeText(requireContext(), "Failed to retrieve user data", Toast.LENGTH_SHORT).show());
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String currentUserUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference userRef = db.collection("users").document(currentUserUid);
+        binding.userImage.setOnClickListener(v -> openImageChooser());
+        loadUserImage();
+
         return root;
+    }
+
+    private void loadUserImage() {
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference imageRef = storageRef.child("profile_images/" + Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+        imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+            Glide.with(requireContext()).load(uri).into(binding.userImage);
+        }).addOnFailureListener(e -> {
+            Toast.makeText(requireContext(), "Failed to load user image", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void openImageChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
     }
 
     @Override
@@ -97,23 +138,10 @@ public class AccountFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
             Uri selectedImageUri = data.getData();
-            // Update the profile photo using the selected image URI
-            updateProfilePhoto(selectedImageUri);
+            Glide.with(this).load(selectedImageUri).into(binding.userImage);
         }
     }
-    private void updateProfilePhoto(Uri imageUri) {
-        Glide.with(this).load(imageUri).into(binding.userImage);
-        String imageName = auth.getCurrentUser().getUid();
-        StorageReference imageRef = storageReference.child(imageName);
-        imageRef.putFile(imageUri)
-                .addOnSuccessListener(taskSnapshot -> {
-                    imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
-                        String downloadUrl = uri.toString();
-                        showToast("Profile photo updated");
-                    }).addOnFailureListener(e -> showToast("Failed to retrieve download URL"));
-                })
-                .addOnFailureListener(e -> showToast("Failed to upload image"));
-    }
+
     private void showToast(String message) {
         Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
     }
